51f33c52cba7903d5b0de2922e963f1928742ab1
ByungjooPark <milkpbj23@gmail.com>
2025-09-15 17:56:13 +0900

    250915 [구현] FESTIVAL_LIST01 리스트 랜더링 처리 및 페이지네이션(스크롤 이벤트)

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,10 +1,11 @@
 const axiosConfig = {
-  serviceKey: 'Sggtbv9ZxiI+NorH9Hn8yBTilwEnaBywr9mqPBUEum0nWykwDK4e/Rsato8S5HqsR2XbKOBclASK/rBPVpWLTg==',
-  MobileOS: 'WEB',
-  MobileApp: 'GreenTravel',
-  type: 'json',
-  arrange: 'O',
-  baseUrl: 'https://apis.data.go.kr/B551011/KorService2',
+  SERVICE_KEY: 'Sggtbv9ZxiI+NorH9Hn8yBTilwEnaBywr9mqPBUEum0nWykwDK4e/Rsato8S5HqsR2XbKOBclASK/rBPVpWLTg==',
+  MOBILE_OS: 'WEB',
+  MOBILE_App: 'GreenTravel',
+  TYPE: 'json',
+  ARRANGE: 'O',
+  BASE_URL: 'https://apis.data.go.kr/B551011/KorService2',
+  NUM_OF_ROWS: 9,
 }
 
 export default axiosConfig;
--- src/store/thunks/festivalThunk.js
+++ src/store/thunks/festivalThunk.js
@@ -4,21 +4,22 @@ import axios from "axios";
 
 const festivalIndex = createAsyncThunk(
   'festivalSlice/festivalIndex',
-  async () => {
-    const url = `${axiosConfig.baseUrl}/searchFestival2`;
+  async (page) => {
+    const url = `${axiosConfig.BASE_URL}/searchFestival2`;
     const config = {
       params: {
-        serviceKey: axiosConfig.serviceKey,
-        MobileOS : axiosConfig.MobileOS,
-        MobileApp: axiosConfig.MobileApp,
-        _type: axiosConfig.type,
-        arrange: axiosConfig.arrange,
-        eventStartDate: '20250401'
+        serviceKey: axiosConfig.SERVICE_KEY,
+        MobileOS : axiosConfig.MOBILE_OS,
+        MobileApp: axiosConfig.MOBILE_App,
+        _type: axiosConfig.TYPE,
+        arrange: axiosConfig.ARRANGE,
+        numOfRows: axiosConfig.NUM_OF_ROWS,
+        pageNo: page,
+        eventStartDate: '20250401' // TODO: 고쳐야 된다.
       }
     }
 
     const response = await axios.get(url, config);
-
     return response.data.response.body;
   }
 );
